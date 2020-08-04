43d940d5cbb8ab38467651a3c2cae125807be64d
JX <jxie1995@gmail.com>
2020-08-03 20:56:47 -0700

    Make changes for production deployment: npm script to build react app after deploying to heroku, add get route to serve static files (frontend) and index.html, production env variables not committed to github

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send('api works'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname + '/client/build/index.html')
+    });
+}
 
 const PORT = process.env.PORT || '3000';
 app.listen(PORT, err => {
